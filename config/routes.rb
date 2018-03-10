d25e24889a3dbb9b893cf732c7d5988e4a87bed6
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-03-10 13:06:35 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :commerces, path: 'commerces', controllers: { sessions: 'commerces/sessions' }
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
