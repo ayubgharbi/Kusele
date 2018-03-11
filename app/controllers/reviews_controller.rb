2612710caa1fe383a229862a09b967770c878e05
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-03-11 13:57:07 +0000

    add geocoder

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_commerce
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def new
     @review = Review.new
--- app/models/commerce.rb
+++ app/models/commerce.rb
@@ -4,7 +4,7 @@ class Commerce < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, :commerce_name, :address, :phone_number, :latitude, :longitude, :register_commerce, :logo, presence: true
+  validates :first_name, :last_name, :commerce_name, :address, :phone_number, :register_commerce, :logo, presence: true
   has_many :products
   has_many :reviews
 
@@ -17,4 +17,7 @@ class Commerce < ApplicationRecord
 		:path => ":rails_root/public/system/:image/:id/:style/:filename",
     	:url => "/system/:image/:id/:style/:filename"
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+    geocoded_by :address
+    after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }  
 end
